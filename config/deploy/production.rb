872da8df11217f66e0a156263a70cc4627b0f0bd
Tinette Nolan <tinette@onthatnote.co.za>
2020-06-11 14:04:12 +0200

    production config for new branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-server 'i2ifacility.org', user: 'ubuntu', roles: %w{web app db}, primary: true
+server 'ec2-54-187-128-168.us-west-2.compute.amazonaws.com', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :ssh_options, {
   forward_agent: true
 }
 
-set :branch, 'master'
+set :branch, 'finmark/develop'
