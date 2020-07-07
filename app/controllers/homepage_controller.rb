73ff556b064bb408c14945f74f1e4377d46e1ff4
Tinette Nolan <tinette@onthatnote.co.za>
2020-07-07 16:13:07 +0200

    home page dropdowns

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -8,6 +8,9 @@ class HomepageController < ApplicationController
       records << klass.published.featured
     end
 
+    @topics = Tag.all.order(:slug)
+    @countries = Country.ordered_by_name
+    
     @tweets = TwitterApi.get_tweets
     @insights = records.flatten.sort do |a, b|
       [[a.featured_position ? 0 : 1, a.featured_position ? a.featured_position.position : nil], b[:updated_at]] <=> [[b.featured_position ? 0 : 1, b.featured_position ? b.featured_position.position : nil], a[:updated_at]]
